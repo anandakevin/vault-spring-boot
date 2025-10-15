71702c37ed8e41daca48520b254d7d83cf5e57ec
Ananda Kevin Refaldo Sariputra <anandakevinsariputra@gmail.com>
2025-10-15 22:13:31 +0700

    feat: add logging examples in UserController inside simple-crud-with-logging

--- simple-crud-with-logging/src/main/java/com/example/demo/controller/UserController.java
+++ simple-crud-with-logging/src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,35 +24,40 @@ import com.example.demo.service.UserService;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping("")
     public ResponseEntity<List<User>> getAllUsers() {
+        logger.info("Fetching all users");
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable UUID id) {
-        return new ResponseEntity<>((User)userService.getUserById(id), HttpStatus.OK);
+        logger.info("Fetching user with id {}", id);
+        return new ResponseEntity<>((User) userService.getUserById(id), HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<User> postMethodName(@RequestBody User createUser) {
+    public ResponseEntity<User> createUser(@RequestBody User createUser) {
+        logger.info("Creating user with name {}", createUser.getName());
         return new ResponseEntity<>(userService.createUser(createUser), HttpStatus.CREATED);
     }
-    
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable UUID id, @RequestBody User updatedUser) {
+        logger.info("Updating user with id {}", updatedUser.getId());
         return new ResponseEntity<>(userService.updateUser(id, updatedUser), HttpStatus.OK);
     }
-    
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUserEntity(@PathVariable UUID id) {
+        logger.info("Deleting user with id {}", id);
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
-    
+
 }
